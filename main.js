120dacf37f607cdad61399be8471bc7d9ba07ec5
Kyle Amoroso <kamoroso1994@gmail.com>
2018-02-25 01:13:06 -0500

    Testing BitArray

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+import BitArray from './BitArray.js';
+
+const bitArray = new BitArray(Uint16Array.of(0b11100100,0b11100001));
+for(const bit of bitArray) {
+  console.log(bit);
+}
